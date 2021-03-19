ae7f5ef1d1d58385530cbe5d9e867ff1ac81f4d5
Your Name <zaadazz66@gmail.com>
2021-03-19 22:00:00 +0200

    first commit

---
+++ text.py
@@ -0,0 +1,36 @@
+handle = open('text.txt')
+
+def revword (word):
+    str_1=" "
+    i = len(word) 
+    while i > 0: 
+        str_1 =str_1+word[ i - 1 ]
+        i = i - 1 
+    return str_1.casefold().strip()
+
+
+def countword() :
+    handle = open('text.txt')
+    inp=handle.read()
+    words=inp.split()
+    word=words[0].casefold()
+    wordi=words[1:]
+    rev=" "
+    counter=1
+    for i in wordi:
+        rev=revword(i).casefold().strip()
+        if rev==word:
+            counter=counter+1
+    return int(counter)
+
+
+def fixtext () :
+   num=countword()
+   return num
+
+fixtext()
+
+
+   
+    
+    
